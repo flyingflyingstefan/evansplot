3eeb78fcf0b1db6ff89b71013c182067eba13247
Stefan Gregory <stefan.gregory@gmail.com>
2025-05-27 11:50:25 +1000

    added this commandline script example

---
+++ examples/example_ndvi.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+import xarray as xr
+import sys
+sys.path.append("../src") # path to evansplot src dir
+import evansplot
+
+ndvi = xr.open_dataset("ndvi_time.nc")
+f = evansplot.evans_plot(ndvi, 
+                     hue_var="ndvi_timing", 
+                     sat_var="ndvi_max", 
+                     min_sat=0, max_sat=0.7,
+                     title = "AVHRR NDVImax Timing",
+                     hue_offset_deg = 30,
+                     hue_value_labels=evansplot.LABELS_MONTHS, 
+                     cbar="cyclic")
+
+plt.savefig("AVHRR_NDVImax_Timing.png", dpi=300)
